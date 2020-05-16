3d1f269cfd80e8ecc297cce340b8bcf30443009f
MCJoeG <1121739073@qq.com>
2020-05-17 00:41:52 +0800

    初始化提交

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+    <mt-button @click.native="testClick" style="background:lightblue">点击我测试</mt-button>
+  </div>
+</template>
+
+<script>
+// export default {}
+// document.addEventListener('DOMContentLoaded',() => {
+// })
+import HelloWorld from './components/HelloWorld'
+import {Toast} from 'mint-ui'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    testClick () {
+      Toast({
+        message: 'sad',
+        duration: 5000
+      })
+    }
+  }
+}
+document.addEventListener('DOMContentLoader', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import store from 'vuex'
+import router from 'vue-router'
+import './assets/styles/icon.css'
+import './assets/fonts/daysOne.css'
+import {Button} from 'mint-ui'
+
+Vue.component(Button.name, Button)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
